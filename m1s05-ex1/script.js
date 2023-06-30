2045b16d452e8093240943e3ee97279e4d74362c
nic4cio <gabrielnn_1@live.com>
2023-06-30 20:00:39 -0300

    age rating exercise

---
+++ m1s05-ex1/script.js
@@ -0,0 +1,13 @@
+let idade = 21;
+
+if(idade <= 12) {
+    console.log("criança");
+} 
+else if (idade <= 17) {
+    console.log("adolescente");
+} 
+else if (idade <= 59) {
+    console.log("adulto");
+} else {
+    console.log("idoso");
+}
