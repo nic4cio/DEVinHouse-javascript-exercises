28ccc92613808f81cb85845657240582965e7347
nic4cio <gabrielnn_1@live.com>
2023-06-30 20:11:40 -0300

    color translation exercise

---
+++ m1s05-ex2/script.js
@@ -0,0 +1,21 @@
+let cor = 'gre';
+
+switch(cor) {
+    case 'blue':
+        console.log('azul');
+        break;
+    case 'red':
+        console.log('vermelho');
+        break;
+    case 'green':
+        console.log('verde');
+        break;
+    case 'purple':
+        console.log('roxo');
+        break;
+    case 'grey':
+        console.log('cinza');
+        break;
+    default:
+        console.log('cor não identificada');
+}
